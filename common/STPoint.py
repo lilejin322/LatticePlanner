b330d0a35ede52ed3751784caedd80c62f84a902
LI LEJIN <707765315@qq.com>
2024-07-17 15:45:01 +0900

    implement STPoint

--- common/STPoint.py
+++ common/STPoint.py
@@ -1,4 +1,88 @@
 from common.Vec2d import Vec2d
+from copy import deepcopy
 
 class STPoint(Vec2d):
-    pass
+    """
+    STPoint class
+    x-axis: t; y-axis: s.
+    """
+
+    def __init__(self, *args):
+        """
+        Constructor
+
+        :param float s: s-coordinate
+        :param float t: t-coordinate
+        """
+
+        if len(args) == 1 and isinstance(args[0], Vec2d):
+            self.__dict = deepcopy(args[0].__dict)
+        elif len(args) == 2:
+            s, t = args
+            super().__init__(t, s)
+        else:
+            raise ValueError("Invalid arguments")
+    
+    @property
+    @staticmethod
+    def x():
+
+        raise AttributeError("x attribute in class STPoint is deleted")
+
+    @property
+    @staticmethod
+    def y():
+
+        raise AttributeError("y attribute in class STPoint is deleted")
+
+    @property
+    def s(self) -> float:
+        """
+        get s-coordinate
+
+        :return: s-coordinate
+        :rtype: float
+        """
+
+        return self._y
+
+    @property
+    def t(self) -> float:
+        """
+        get t-coordinate
+
+        :return: t-coordinate
+        :rtype: float
+        """
+
+        return self._x
+
+    @s.setter
+    def s(self, s: float) -> None:
+        """
+        s setter
+
+        :param float s: s-coordinate
+        """
+
+        self._y = s
+    
+    @t.setter
+    def t(self, t: float) -> None:
+        """
+        t setter
+
+        :param float t: t-coordinate
+        """
+
+        self._x = t
+
+    def __str__(self) -> str:
+        """
+        Returns a human-readable string representing this object
+
+        :returns: human-readable string
+        :rtype: str
+        """
+
+        return f"STPoint(s: {self._y}, t: {self._x})"
